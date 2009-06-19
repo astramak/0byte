03c1e461bbd7dd73c0034aacaef00f66b2c61347
vgarvardt <vgarvardt@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-06-19 21:58:58 +0000

    - code refactoring; - db layer replacements; - templates formatting; - removed unused file;

--- trunk/style/templates/frames/edit_comment.tpl.php
+++ trunk/style/templates/frames/edit_comment.tpl.php
@@ -1,9 +1,14 @@
 <h3>Редактирование комментария</h3>
 <div id="mq" class="inpt"></div>
-<form method='post' name='new'
-	action='twork.php?wt=editcom&id=<?php echo($id); ?>'><textarea
-	onkeypress="do_key(this.form,'new',event);"
-	onkeydown="if('\v'=='v') {do_key(this.form,'new',event);}" name='text'
-	rows='10' cols='80'><?php echo $text; ?></textarea><br />
-<input type='submit' value='Редактировать' /></form>
+<form method='post' name='new' action='twork.php?wt=editcom&id=<?php echo($id); ?>'>
+	<div>
+		<textarea
+			onkeypress="do_key(this.form,'new',event);"
+			onkeydown="if('\v'=='v') {do_key(this.form,'new',event);}" name='text'
+			rows='10' cols='80'><?php echo $text; ?></textarea>
+	</div>
+	<div>
+		<input type='submit' value='Редактировать' />
+	</div>
+</form>
 <script type="text/javascript"> mk('mq','new'); </script>
--- trunk/style/templates/top/top.tpl.php
+++ trunk/style/templates/top/top.tpl.php
@@ -1,6 +1,6 @@
 <div id="top">
-<div id="h1">
-<h1><a href="<?php echo $site; ?>"><?php echo $s_name; ?></a></h1>
-<div id="banner"></div>
-</div>
-<div class="mf">
+	<div id="h1">
+		<h1><a href="<?php echo $site; ?>"><?php echo $s_name; ?></a></h1>
+		<div id="banner"></div>
+	</div>
+	<div class="mf">
