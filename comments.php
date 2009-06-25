20bb8fef88b525d27857fad1b409fa1d3e15781a
vgarvardt <vgarvardt@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-06-25 18:20:10 +0000

    couple minor fixes

--- comments.php
+++ comments.php
@@ -13,29 +13,29 @@ include("inc/top.php");
 		$alien=new user;
 		$alien->find($who);
 		$ssa = "<img class='cnoauth' src='style/img/figure.gif' />";
-		if (strlen($alien->av)>2) {
-			$big_img="<img src='res.php?t=av&img=".$alien->av."' style='float:left;' alt='".$alien->login."' />";
+		$big_img = '';
+		if (strlen($alien->av)> 2 ) {
+			$big_img = "<img src='res.php?t=av&img=".$alien->av."' style='float:left;' alt='".$alien->login."' />";
 			$ssa = "<img class='cauth' src='res.php?t=av&img=".$alien->av."' style='float:left;' alt='".$alien->login."' />";
 		}
-		if ($alien->rate()>0) {
+		if ($alien->rate() > 0) {
 			$rtp="<span class='rp'>".$alien->rate()."</span>";
-		} elseif ($alien->rate()<0) {
-			$rtp="<span class='rm'>".$alien->rate()."</span>";
+		} elseif ($alien->rate() < 0) {
+			$rtp = "<span class='rm'>".$alien->rate()."</span>";
 		}  else {
-			$rtp=$alien->rate();
+			$rtp = $alien->rate();
 		}
 		echo "<div id='btop'>".$big_img."<span class='bnm'><a href='user/".$alien->login."'>".$alien->login."</a></span><span class='rate'>
-			".$in."     <a class='ratep' href='twork.php?wt=rateuser&name=".$alien->login."&rate=p&from=".$cur."'>+</a>
-                ".$rtp."<a class='ratem' href='twork.php?wt=rateuser&name=".$alien->login."&rate=m&from=".$cur."'>&ndash;</a></span></div>";
+				<a class='ratep' href='twork.php?wt=rateuser&name=".$alien->login."&rate=p&from=".$cur."'>+</a>
+				".$rtp."<a class='ratem' href='twork.php?wt=rateuser&name=".$alien->login."&rate=m&from=".$cur."'>&ndash;</a></span></div>";
 
 		$result = db_query('SELECT * FROM comment WHERE who = %s ORDER BY id DESC', $who);
 		if (!db_num_rows($result)) {
 			echo "<span id='nocom'>Коментариев нет</span>";
 		} else {
-			$com=new com();
 			while($row = db_fetch_assoc($result)) {
-				$com->make($row);
-
+				$com = new com($row);
+				
 				$post = db_fetch_assoc(db_query('SELECT * FROM post WHERE id = %d', $row['krnl']));
 				if ($post['blogid']) {
 					$us="<a href='blog/".$post['blogid']."'>".$post['blog']."</a>  &#8212;  ";
