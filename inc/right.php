a0c9a5b6f33eabad6631827de6d93d2971aad25d
nvbn.rm <nvbn.rm@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-08-06 09:21:00 +0000

    Draft fully work right now! =) Give me icon to draft!!!!!!

--- inc/right.php
+++ inc/right.php
@@ -20,9 +20,9 @@ $tarr = array();
 $result = db_query('SELECT * FROM `post` WHERE `blck` = 0 && `lock` = 0 ORDER BY id DESC LIMIT 16');
 $e += db_num_rows($result);
 while ($row = db_fetch_assoc($result)) {
-	$row['type'] = 'post';
-	$row['rate'] = $row['ratep'] = $row['ratem'];
-	$tarr[] = $row;
+    $row['type'] = 'post';
+    $row['rate'] = $row['ratep'] = $row['ratem'];
+    $tarr[] = $row;
 }
 
 $result = db_query('SELECT *, p.id AS pid, p.date as pate
@@ -33,60 +33,60 @@ $result = db_query('SELECT *, p.id AS pid, p.date as pate
 					LIMIT 16');
 $e += db_num_rows($result);
 while ($row = db_fetch_assoc($result)) {
-	$row['type'] = 'comment';
-	$row['rate'] = $row['ratep'] = $row['ratem'];
-	$tarr[] = $row;
+    $row['type'] = 'comment';
+    $row['rate'] = $row['ratep'] = $row['ratem'];
+    $tarr[] = $row;
 }
 
 $siz = count($tarr) - 1;
 for ($i = $siz; $i >= 0; $i--) {
-	for ($j = 0; $j <= ($i-1); $j++) {
-		if ($tarr[$j]['date'] < $tarr[$j+1]['date']) {
-			$k = $tarr[$j];
-			$tarr[$j] = $tarr[$j+1];
-			$tarr[$j+1] = $k;
-		}
-	}
+    for ($j = 0; $j <= ($i-1); $j++) {
+        if ($tarr[$j]['date'] < $tarr[$j+1]['date']) {
+            $k = $tarr[$j];
+            $tarr[$j] = $tarr[$j+1];
+            $tarr[$j+1] = $k;
+        }
+    }
 }
 echo render_hands_free($tarr,$siz);
 echo render_search_panel();
 
 if (!($tops = readCache('tops.cache', 30))) {
-	$tops = '';
-	$result = db_query('SELECT * FROM tags WHERE num > 0 ORDER BY num DESC LIMIT 40');
-	$tags = array();
-	while ($row = db_fetch_assoc($result)) {
-		if (!isset($max)) $max = $row['num'];
-		$row['weight'] = ceil($row['num'] / $max * 10);
-		$tags[] = $row;
-	}
-	shuffle($tags);
-	$tops .= render_tags($tags);
+    $tops = '';
+    $result = db_query('SELECT * FROM tags WHERE num > 0 ORDER BY num DESC LIMIT 40');
+    $tags = array();
+    while ($row = db_fetch_assoc($result)) {
+        if (!isset($max)) $max = $row['num'];
+        $row['weight'] = ceil($row['num'] / $max * 10);
+        $tags[] = $row;
+    }
+    shuffle($tags);
+    $tops .= render_tags($tags);
 
-	$result = db_query('SELECT *, (ratep - ratem) AS rate FROM blogs ORDER BY rate DESC LIMIT 10');
-	$blogs = array();
-	while ($row = db_fetch_assoc($result)) {
-		$blogs[] = $row;
-	}
+    $result = db_query('SELECT *, (ratep - ratem) AS rate FROM blogs ORDER BY rate DESC LIMIT 10');
+    $blogs = array();
+    while ($row = db_fetch_assoc($result)) {
+        $blogs[] = $row;
+    }
 
-	$result = db_query('SELECT *, (ratep - ratem + prate / %f + crate / %f + brate / %f) AS rate  FROM users WHERE lvl = 0 ORDER BY rate DESC LIMIT 10', $post_r, $com_r, $blog_r);
-	$users = array();
-	while ($row = db_fetch_assoc($result)) {
-		$users[] = $row;
-	}
-	$tops .= render_tops($users, $blogs);
-	writeCache($tops,'tops.cache');
+    $result = db_query('SELECT *, (ratep - ratem + prate / %f + crate / %f + brate / %f) AS rate  FROM users WHERE lvl = 0 ORDER BY rate DESC LIMIT 10', $post_r, $com_r, $blog_r);
+    $users = array();
+    while ($row = db_fetch_assoc($result)) {
+        $users[] = $row;
+    }
+    $tops .= render_tops($users, $blogs);
+    writeCache($tops,'tops.cache');
 }
 echo $tops;
 
 $result = db_query('SELECT name FROM users WHERE online >= %d ORDER BY online DESC', time() - 300);
 while ($row = db_fetch_assoc($result)) {
-	$onlines[] = $row['name'];
+    $onlines[] = $row['name'];
 }
 
 $result = db_query('SELECT name FROM users ORDER BY id DESC LIMIT 5');
 while ($row = db_fetch_assoc($result)) {
-	$news[] = $row['name'];
+    $news[] = $row['name'];
 }
 
 echo render_online_and_new($onlines, $news);
@@ -103,35 +103,38 @@ var cd=null;
 }';
 $e=0;
 if (isset($_SESSION['tp1'])) {
-	if ($_SESSION['tp1']=='pst') {
-		$scr.="g_plist('post'";
-		$e=1;
-	} elseif ($_SESSION['tp1']=='com') {
-		$scr.="g_plist('com'";
-		$e=1;
-	} elseif ($_SESSION['tp1']=='eye') {
-		$scr.="g_plist('eye'";
-		$e=1;
-	}
+    if ($_SESSION['tp1']=='pst') {
+        $scr.="g_plist('post'";
+        $e=1;
+    } else if ($_SESSION['tp1']=='com') {
+        $scr.="g_plist('com'";
+        $e=1;
+    } else if ($_SESSION['tp1']=='eye') {
+        $scr.="g_plist('eye'";
+        $e=1;
+    } else if ($_SESSION['tp1']=='draft') {
+        $scr.="g_plist('draft'";
+        $e=1;
+    }
 
 }
 
 if (isset($_SESSION['tp2'])) {
-	if ($_SESSION['tp2']=='us') {
-		if ($e==1) {
-			$scr.=",1,'top_user');";
-			$e=2;
-		} else {
-			$scr.="g_plist('top_user');";
-		}
-	} elseif ($_SESSION['tp2']=='bl') {
-		if ($e==1) {
-			$scr.=",1,'top_blog');";
-			$e=2;
-		} else {
-			$scr.="g_plist('top_blog');";
-		}
-	}
+    if ($_SESSION['tp2']=='us') {
+        if ($e==1) {
+            $scr.=",1,'top_user');";
+            $e=2;
+        } else {
+            $scr.="g_plist('top_user');";
+        }
+    } elseif ($_SESSION['tp2']=='bl') {
+        if ($e==1) {
+            $scr.=",1,'top_blog');";
+            $e=2;
+        } else {
+            $scr.="g_plist('top_blog');";
+        }
+    }
 }
 if ($e==1) {$scr.=");";}
 $script->add($scr);
--- js/right.js
+++ js/right.js
@@ -3,134 +3,150 @@ var fl_pid=0; var fl_cid=0;
 var fl_u=0; var fl_b=0;
 var xy=document.getElementById("alist");
 function g_plist(tp) {
-	var xmlhttp = getXmlHttp();
-	if (tp=='post') {
-		ttp='pst';
-		if (fl_p==0) {
-			tt="&lm=16";
-			fl_p=1;
-			nw=1;
-		} else {
-			tt='&aft='+fl_pid;
-			nw=0;
-		}
-		wh=document.getElementById('plist');
-		document.getElementById('cf').style.display="none";
-		document.getElementById('af').style.display="none";
-		document.getElementById('ef').style.display="none";
-		document.getElementById('pf').style.display="table-row";
-	} else if (tp=='com') {
-		ttp='com';
-		if (fl_c==0) {
-			tt="&lm=16";
-			fl_c=1;
-			nw=1;
-		} else {
-			tt='&aft='+fl_cid;
-			nw=0;
-		}
-		wh=document.getElementById('clist');
-		document.getElementById('cf').style.display="table-row";
-		document.getElementById('af').style.display="none";
-		document.getElementById('ef').style.display="none";
-		document.getElementById('pf').style.display="none";
-	} else if (tp=='top_user') {
-		ttp='top_user';
-		tt="&lm=16";
-		fl_u=0;
-		nw=1;
-		document.getElementById("bltop").style.display="none";
-		document.getElementById("ustop").style.display="table-row";
-		document.getElementById("shuser").style.display="block";
-		wh=document.getElementById("ulister");
-		wh.innerHTML="";
-	} else if (tp=='top_blog') {
-		ttp='top_blog';
-		tt='&lm=16';
-		nw=1;
-		document.getElementById("bltop").style.display="table-row";
-		document.getElementById("ustop").style.display="none";
-		document.getElementById("shblog").style.display="block";
-		document.getElementById("shall").style.display="none";
-		wh=document.getElementById("blist");
-		wh.innerHTML="";
-	} else if (tp=='eye') {
-		ttp='eye';
-		if (fl_e==0) {
-			tt="&lm=16";
-			fl_e=1;
-			nw=1;
-		} else {
-			tt='&no';
-			nw=0;
-		}
-		wh=document.getElementById('eblist');
-		document.getElementById('ef').style.display="table-row";
-		document.getElementById('af').style.display="none";
-		document.getElementById('cf').style.display="none";
-		document.getElementById('pf').style.display="none";
-	}
-	xmlhttp.open('GET', "ajax/r_get?"+ttp+tt, true);
-	var atr="";
-	if (arguments[1]==1) {
-		atr="g_plist('"+arguments[2]+"')";
-	}
+    var xmlhttp = getXmlHttp();
+    if (tp=='post') {
+        ttp='pst=1';
+        if (fl_p==0) {
+            tt="&lm=16";
+            fl_p=1;
+            nw=1;
+        } else {
+            tt='&aft='+fl_pid;
+            nw=0;
+        }
+        wh=document.getElementById('plist');
+        document.getElementById('cf').style.display="none";
+        document.getElementById('af').style.display="none";
+        document.getElementById('ef').style.display="none";
+        document.getElementById('pf').style.display="table-row";
+        document.getElementById('df').style.display="none";
+    } else if (tp=='com') {
+        ttp='com=1';
+        if (fl_c==0) {
+            tt="&lm=16";
+            fl_c=1;
+            nw=1;
+        } else {
+            tt='&aft='+fl_cid;
+            nw=0;
+        }
+        wh=document.getElementById('clist');
+        document.getElementById('cf').style.display="table-row";
+        document.getElementById('af').style.display="none";
+        document.getElementById('ef').style.display="none";
+        document.getElementById('pf').style.display="none";
+        document.getElementById('df').style.display="none";
+    } else if (tp=='top_user') {
+        ttp='top_user=1';
+        tt="&lm=16";
+        fl_u=0;
+        nw=1;
+        document.getElementById("bltop").style.display="none";
+        document.getElementById("ustop").style.display="table-row";
+        document.getElementById("shuser").style.display="block";
+        wh=document.getElementById("ulister");
+        wh.innerHTML="";
+    } else if (tp=='top_blog') {
+        ttp='top_blog=1';
+        tt='&lm=16';
+        nw=1;
+        document.getElementById("bltop").style.display="table-row";
+        document.getElementById("ustop").style.display="none";
+        document.getElementById("shblog").style.display="block";
+        document.getElementById("shall").style.display="none";
+        wh=document.getElementById("blist");
+        wh.innerHTML="";
+    } else if (tp=='eye') {
+        ttp='eye=1';
+        if (fl_e==0) {
+            tt="&lm=16";
+            fl_e=1;
+            nw=1;
+        } else {
+            tt='&no';
+            nw=0;
+        }
+        wh=document.getElementById('eblist');
+        document.getElementById('ef').style.display="table-row";
+        document.getElementById('af').style.display="none";
+        document.getElementById('cf').style.display="none";
+        document.getElementById('pf').style.display="none";
+        document.getElementById('df').style.display="none";
+    } else if (tp=='draft') {
+        ttp='draft=1';
+        tt="&lm=16";
+        nw=1;
+        wh=document.getElementById('drlist');
+        document.getElementById('df').style.display="table-row";
+        document.getElementById('af').style.display="none";
+        document.getElementById('cf').style.display="none";
+        document.getElementById('pf').style.display="none";
+        document.getElementById('ef').style.display="none";
+    }
+    xmlhttp.open('GET', "ajax/r_get?"+ttp+tt, true);
+    var atr="";
+    if (arguments[1]==1) {
+        atr="g_plist('"+arguments[2]+"')";
+    }
 	
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4) {
-			var resp=eval('('+xmlhttp.responseText+')');
-			for (i=0;i<=resp.num-1;i++) {
-				rt="";
-				ln="";
-				if (resp.arr[i].rate>0) {
-					rt="<span class='scb'>(<span class='rp'>"+resp.arr[i].rate+"</span>)</span>";
-				} else if (resp.arr[i].rate<0) {
-					rt="<span class='scb'>(<span class='rm'>"+resp.arr[i].rate+"</span>)</span>";
-				}
-				if (tp=="com") {
-					if (resp.arr[i].id>fl_cid){
-						fl_cid=resp.arr[i].id;
-					}
-					what='<li class="celis"><a href="user/'+resp.arr[i].who+'/">'+resp.arr[i].who+'</a> &#8212; &laquo;<a href="post/'+resp.arr[i].wid+'/#cmnt'+resp.arr[i].id+'">'+resp.arr[i].blg+' / '+resp.arr[i].where+'</a>&raquo; '+rt+'</li>';
-				} else if (tp=='post'){
-					if (resp.arr[i].id>fl_pid) {
-						fl_pid=resp.arr[i].id;
-					}
-					what='<li class="pelis"><a href="'+escape(resp.arr[i].url)+'">'+resp.arr[i].who+'</a> &#8212; &laquo;<a href="post/'+resp.arr[i].id+'/">'+resp.arr[i].title+'</a>&raquo; '+rt+'</li>';
-				} else if (tp=='top_user') {
-					what='<li><a href="user/'+resp.arr[i].name+'/">'+resp.arr[i].name+'</a> '+rt+'</li>';
-				} else if (tp=='top_blog') {
-					what='<li><a href="blog/'+resp.arr[i].id+'/">'+resp.arr[i].name+'</a> '+rt+'</li>';
-				} else if (tp=='eye') {
-					what='<li ><a href="'+escape(resp.arr[i].url)+'">'+resp.arr[i].who+'</a> &#8212; &laquo;<a href="post/'+resp.arr[i].id+'/">'+resp.arr[i].title+'</a>&raquo;</li>';
-				}
-				if (nw==0) {
-					xy.innerHTML=what+xy.innerHTML;
-					wh.innerHTML=what+wh.innerHTML;
-				} else {
-					wh.innerHTML+=what;
-				}
-			}
-			eval(atr);
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4) {
+            var resp=eval('('+xmlhttp.responseText+')');
+            for (i=0;i<=resp.num-1;i++) {
+                rt="";
+                ln="";
+                if (resp.arr[i].rate>0) {
+                    rt="<span class='scb'>(<span class='rp'>"+resp.arr[i].rate+"</span>)</span>";
+                } else if (resp.arr[i].rate<0) {
+                    rt="<span class='scb'>(<span class='rm'>"+resp.arr[i].rate+"</span>)</span>";
+                }
+                if (tp=="com") {
+                    if (resp.arr[i].id>fl_cid){
+                        fl_cid=resp.arr[i].id;
+                    }
+                    what='<li class="celis"><a href="user/'+resp.arr[i].who+'/">'+resp.arr[i].who+'</a> &#8212; &laquo;<a href="post/'+resp.arr[i].wid+'/#cmnt'+resp.arr[i].id+'">'+resp.arr[i].blg+' / '+resp.arr[i].where+'</a>&raquo; '+rt+'</li>';
+                } else if (tp=='post'){
+                    if (resp.arr[i].id>fl_pid) {
+                        fl_pid=resp.arr[i].id;
+                    }
+                    what='<li class="pelis"><a href="'+escape(resp.arr[i].url)+'">'+resp.arr[i].who+'</a> &#8212; &laquo;<a href="post/'+resp.arr[i].id+'/">'+resp.arr[i].title+'</a>&raquo; '+rt+'</li>';
+                } else if (tp=='top_user') {
+                    what='<li><a href="user/'+resp.arr[i].name+'/">'+resp.arr[i].name+'</a> '+rt+'</li>';
+                } else if (tp=='top_blog') {
+                    what='<li><a href="blog/'+resp.arr[i].id+'/">'+resp.arr[i].name+'</a> '+rt+'</li>';
+                } else if (tp=='eye') {
+                    what='<li><a href="'+escape(resp.arr[i].url)+'">'+resp.arr[i].who+'</a> &#8212; &laquo;<a href="post/'+resp.arr[i].id+'/">'+resp.arr[i].title+'</a>&raquo;</li>';
+                } else if (tp=='draft') {
+                    what='<li><a href="'+escape(resp.arr[i].url)+'">'+escape(resp.arr[i].title)+'</a></li>';
+                }
+                if (nw==0) {
+                    xy.innerHTML=what+xy.innerHTML;
+                    wh.innerHTML=what+wh.innerHTML;
+                } else {
+                    wh.innerHTML+=what;
+                }
+            }
+            eval(atr);
 		
-		}
-	};
-	xmlhttp.send(null);
+        }
+    };
+    xmlhttp.send(null);
 }
 function hplist() {
-	_touch('ajax/r_get?noo=1');
-	document.getElementById('pf').style.display="none";
-	document.getElementById('cf').style.display="none";
-	document.getElementById('ef').style.display="none";
-	document.getElementById('af').style.display="table-row";	
+    _touch('ajax/r_get?noo=1');
+    document.getElementById('pf').style.display="none";
+    document.getElementById('cf').style.display="none";
+    document.getElementById('ef').style.display="none";
+    document.getElementById('df').style.display="none";
+    document.getElementById('af').style.display="table-row";
 }
 function hgptop() {
-	_touch('ajax/r_get?noo=2');
-	document.getElementById("ulister").innerHTML=ulist;
-	document.getElementById("blist").innerHTML=blist;
-	document.getElementById("bltop").style.display="table-row";
-	document.getElementById("ustop").style.display="table-row";
-	document.getElementById("shblog").style.display="none";
-	document.getElementById("shuser").style.display="none";
-	document.getElementById("shall").style.display="block";
+    _touch('ajax/r_get?noo=2');
+    document.getElementById("ulister").innerHTML=ulist;
+    document.getElementById("blist").innerHTML=blist;
+    document.getElementById("bltop").style.display="table-row";
+    document.getElementById("ustop").style.display="table-row";
+    document.getElementById("shblog").style.display="none";
+    document.getElementById("shuser").style.display="none";
+    document.getElementById("shall").style.display="block";
 }
