33e4dfd49761ee3aeeb0c28b366d96a08985042c
nvbn.rm <nvbn.rm@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-08-19 16:09:19 +0000

    fix issue i'll kill netbeans developers

--- trunk/style/templates/frames/single_post.tpl.php
+++ trunk/style/templates/frames/single_post.tpl.php
@@ -15,6 +15,7 @@
         ?>тслеживать</a>)
     <?php } ?>
     (<a id="favor" href="<?php echo $favourite_url; ?>"><?php if (!$favourite) { ?>В избранное<?php } else { ?>Из избранного<?php } ?></a>) <?php } ?>
+    (<a href="like/<?php echo $id; ?>">Похожие</a>)
 <span class='rate'><a class='ratep' href='<?php echo $ratep_url; ?>'>+</a>
     <span id='rp<?php echo $id; ?>'><?php
     if ($rate>0) {
