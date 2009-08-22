2baaa0f5793285cb121b812fcd1e7799fe56935c
astramak <astramak@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-08-22 13:10:43 +0000

    add images (low) + change in user profile template

--- trunk/style/templates/frames/user.tpl.php
+++ trunk/style/templates/frames/user.tpl.php
@@ -11,7 +11,7 @@
     <img class='photo' style='float:right' src='<?php echo $avatar_url; ?>' alt='' />
 <?php }
 if ($use_micro)  { ?>
-<br/><i><span class='grey'><a href="<?php echo $micro_url;?>"><?php echo $micro_name;?></a>:<?php echo $micro_status;?></span></i>
+<br/><i><span class='grey'><?php echo $micro_status;?><a href="<?php echo $micro_url;?>" title='<?php echo $micro_name;?>' style='text-decoration:none'>...</a></span></i>
 <?php }
 if ($blocked) {?>
     <br><h3>Пoльзователь заблокирован</h3>
@@ -21,16 +21,16 @@ if ($blocked) {?>
     <?php echo $lvl; ?>-й уровень доступа<br><br>
 <?php }
 if (!$hide_mail) {?>
-    <img src='http://img200.imageshack.us/img200/5982/emailh.png'/> <a href='mailto:<?php echo $mail;?>' class='email'><?php echo $mail;?></a>, 
+    <img src='style/n_img/mail.png'/> <a href='mailto:<?php echo $mail;?>' class='email'><?php echo $mail;?></a>, 
 <?php } $not_first=0;
 if ($icq['set']) { $not_first=1; ?>
-    <img src='http://web.icq.com/whitepages/online?icq=<?php echo $icq['text'];?>&img=5'/><?php echo $icq['text'];
+    <img src='style/n_img/icq.png'/><?php echo $icq['text'];
 }
-if ($jabber['set']) {  if ($not_first) echo ','; $not_first=1; ?><img src='http://img194.imageshack.us/img194/2286/jabberonline.png'/><a href='xmpp:<?php echo $jabber['text']; ?>'><?php echo $jabber['text']; ?></a><?php
+if ($jabber['set']) {  if ($not_first) echo ','; $not_first=1; ?><img src='style/n_img/jabber.png'/><a href='xmpp:<?php echo $jabber['text']; ?>'><?php echo $jabber['text']; ?></a><?php
  }
-if ($usite['set']) { if ($not_first) echo ','; ?> <img src='http://img44.imageshack.us/img44/4667/webbrowser.png'/> <noindex><a href='<?php echo $usite['text']; ?>' rel='nofollow'><?php echo $usite['text']; ?></a></noindex>
+if ($usite['set']) { if ($not_first) echo ','; ?> <img src='style/n_img/www.png'/> <noindex><a href='<?php echo $usite['text']; ?>' rel='nofollow'><?php echo $usite['text']; ?></a></noindex>
 <?php }
-echo '- контакты.';
+echo '— контакты.';
 ?>
 <br><br>
 <?php 
@@ -40,7 +40,7 @@ if ($city['set']) {?>
 
 <br><br/>
 <?php if ($about['set']) {?>
-    <?php echo $name; ?> о себе: <br><span class='note'><?php echo $about['text']; ?></span>
+О себе: <br><span class='note'><?php echo $about['text']; ?></span>
 <?php }
 if (@$blogs) { ?>
 <br><br>Состоит в
@@ -48,14 +48,14 @@ if (@$blogs) { ?>
         <a href='blog/<?php $blog['id']; ?>/'><?php echo $blog['name']; ?></a><?php if ($blog!=end($blogs)) echo ','; ?>
         <?php } } ?>
  
-<br><br>
-Написал <a href='auth/<?php echo $name; ?>/'><?php echo $post_count; ?> постов</a> и <a href='comment/<?php echo $name; ?>/'><?php echo $comment_count; ?> коментариев</a>
-
 <br>
 <?php if (@$friends) { ?>Дружит с
         <?php foreach ($friends as $friend) { if (strlen($friend)>1) { ?>
             <a href='user/<?php echo $friend; ?>/'><?php echo $friend; ?></a><?php if ($friend!=end($friends)) echo ','; ?>
         <?php } } } ?>
+<br><br>
+Написал <a href='auth/<?php echo $name; ?>/'><?php echo $post_count; ?> постов</a> и <a href='comment/<?php echo $name; ?>/'><?php echo $comment_count; ?> коментариев</a>
+
 <br /><br><hr>
 <?php if ($owner) {?>
     <br /><img src='http://astramak.jino.ru/welinux/edit.png'/> <a href="work/edituser">Редактировать профиль</a> 
