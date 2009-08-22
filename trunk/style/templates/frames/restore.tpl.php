c19f28444d2db4eabf525dcbc80f7ca1a979284a
nvbn.rm <nvbn.rm@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-08-22 13:44:24 +0000

    password restore interface

---
+++ trunk/style/templates/frames/restore.tpl.php
@@ -0,0 +1,5 @@
+<h2>Вы забыли пароль?</h2>
+<form action="looz" method="post">
+    <label>Ваш логин: <input name="nick" type="text" /></label>
+    <br /><input type="submit" value="Отправить запрос" />
+</form>
