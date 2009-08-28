9ff081f0227bf7c3ffbf4e0957b8703cdd2a3856
astramak <astramak@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-08-28 18:54:01 +0000

    final? version of "login" template

--- style/templates/frames/login.tpl.php
+++ style/templates/frames/login.tpl.php
@@ -2,33 +2,31 @@
 	<div id="lgn">
 		<form name="ta" method="post"
 			  action="login.php?cur=<?php echo $current; ?>">
-			<table border="0">
-				<tr>
-					<td>Логин:</td>
-					<td><input type="text" name="login" /></td>
-				</tr>
-				<tr>
-					<td>Пароль:</td>
-					<td><input type="password" name="pwd" /></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><input type="checkbox" name="zap" value="1" />помнить меня</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><input type="submit" value="Войти" /> <?php
+	<table>
+	<tr>
+	<td>
+	<span class="grey">логин</span><br>
+	<input type="text" name="login" tabindex="1"/><br>
+	<a href='register'>регистрация</a><br>
+    </td>
+    <td>
+	<span class="grey">пароль</span><br>
+	<input type="password" name="pwd" tabindex="2"/><br>
+    <a href="looz/">восстановление</a>
+
+	</td></tr></table>
+	<br><input type="checkbox" name="zap" value="1" /><span class="grey">помнить меня</span><br><br>
+			
+	<input type="submit" value="Войти" class="tag_w7" tabindex="3" /> <?php
 						if ($js) {
-							?><input type='button' onblur='onBlur(this)' onfocus='onFocus(this)' onclick='unlogin()' value='Вернуться' />
-					<?php }	?></td>
-				</tr>
-			</table>
+							?><input type='button' onblur='onBlur(this)' onfocus='onFocus(this)' onclick='unlogin()' value='&larr; Вернуться' class="tag_w4"/>
+					<?php }	?>
+
 		</form>
 		<br/>
 		<?php if ($new) { ?>
 		<b>Перед входом активируйте свою учётную запись, для этого проверьте свою электронную почту и пройдите по полученной ссылке!</b>
 		<?php } else { ?>
-		<a href='register'>Зарегистрироваться</a> - <a href="looz/">Вспомнить пароль</a>
 		<?php }
 		if (!$js) {
 			?>
