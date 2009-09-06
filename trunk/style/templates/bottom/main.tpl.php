aa935da023246f6add43efb051918bd822a12e3a
astramak <astramak@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-09-06 18:49:51 +0000

    changes in styles

--- trunk/style/templates/bottom/main.tpl.php
+++ trunk/style/templates/bottom/main.tpl.php
@@ -2,12 +2,12 @@
 </tr>
 </tbody>
 </table>
+</div>
 <div class='mf' id='btmf'>
 <div class='amenuel'><a href="<?php echo $site; ?>"><?php echo $sl_name; ?></a></div>
 <div class='menuel'><a href="all/ar">Правила</a></div>
 <div class='menuel'><a href="all/act">Цели и задачи</a></div> 
 <div class='menuel'><a href="all/help">Справка</a></div>
-</div>
 </div>
 <?php echo $SCRIPT; ?>
 </body>
