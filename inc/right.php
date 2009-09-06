aa6377c2064ac35919b182e114c5b72b1910fc37
nvbn.rm <nvbn.rm@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-09-06 17:32:36 +0000

    bug fix

--- inc/right.php
+++ inc/right.php
@@ -55,7 +55,7 @@ echo render_search_panel();
 if (!($tops = readCache('tops.cache', 30))) {
     $tops = '';
     $result = db_query('SELECT * FROM tags WHERE num > 0 ORDER BY num DESC LIMIT 40');
-    $tops .= render_tags(generate_tag_array($result));
+    $tops .= render_tags(generate_tag_array($result,28,6));
 
     $result = db_query('SELECT *, (ratep - ratem) AS rate FROM blogs ORDER BY rate DESC LIMIT 10');
     $blogs = array();
