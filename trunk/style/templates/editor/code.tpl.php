6dfc1b46d76998582865b1edd902eb27fe9ce193
astramak <astramak@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-09-13 22:30:44 +0000

    tabindex in code, image and link tpls issue 61
    in some lols browsers dnt work =( why? :D

--- trunk/style/templates/editor/code.tpl.php
+++ trunk/style/templates/editor/code.tpl.php
@@ -1,3 +1,3 @@
 <h3>Код</h3>
-<form onsubmit="codea(this,'<?php echo $fr; ?>','<?php echo $el; ?>'); return false;"><label>Язык для подсветки: <input type='text' name='code' /></label>
+<form onsubmit="codea(this,'<?php echo $fr; ?>','<?php echo $el; ?>'); return false;"><label>Язык для подсветки:</label> <input type='text' name='code' tabindex='1' />
 <input type='submit' value='Создать' /><input type='button' onClick='a_cr()' value='Отмена' /></form>
--- trunk/style/templates/editor/link.tpl.php
+++ trunk/style/templates/editor/link.tpl.php
@@ -1,5 +1,5 @@
 <h3>Ссылка</h3>
 <form onsubmit="urla(this,'<?php echo $fr;?>','<?php echo $el;?>'); return false;">
-      <label>Адрес <input type='text' name='url' /></label>
+      <label>Адрес <input type='text' name='url' tabindex='1' /></label>
 	<input type='submit' value='Создать' /><input type='button' onClick='a_cr()' value='Отмена' />
 </form>
