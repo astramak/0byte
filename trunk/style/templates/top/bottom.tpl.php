a09fbd8433021313e5450553334e334a5ca4757e
nvbn.rm <nvbn.rm@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-09-14 19:02:47 +0000

    color, yeah =)

--- trunk/style/templates/top/bottom.tpl.php
+++ trunk/style/templates/top/bottom.tpl.php
@@ -1,19 +1,19 @@
 <div class="usln">
-	<div class="usrd">
-		<?php if ($loged) {?>
-		<a class='you' href='user/'><?php echo $login;?></a>
-		<span <?php if ($user_rate>0) echo "class='rp'"; else  if ($user_rate<0)  echo "class='rm'";?>><?php echo $user_rate;?>
-		</span><?php if ($allow_post) { ?><a href='work/newpost'>Новый пост</a>
-			<?php } if ($allow_blog) {?> / <a href='work/newblog'>блог</a><?php }?>. Ваши <a href='work/myblog'>блоги</a> и
-		 <!-- <img src='style/img/<?php if ($not_readed>0) {?>mail<?php } else {?>envelope<?php }?>.gif'
-					 alt='' /> --> <a href='work/pmls' title='<?php echo $not_readed." / ".$mail;?>'>ЛС</a><span class='grey'> (<?php echo $not_readed."/".$mail;?>)</span> </div>
-		<div id='inout'><img src='style/img/figure.gif' alt='' />
-			<form id='out' method='post' action='<?php echo $site;?>'><input type='hidden' name='un' value='1' />
-				<input style="cursor: pointer;"  id='outb' type='submit' value='Выйти' /></form></div>
-		<?php } else {?>
-		<a href='register'>Зарегистрироваться</a></div>
-	<div id='inout'><a id='lgin' href='login/<?php echo $current_url; ?>'>Войти</a></div>
-	<?php }?>
+    <div class="usrd">
+        <?php if ($loged) {?>
+        <a class='you' href='user/'><?php echo $login;?></a>
+        <span <?php if ($user_rate>0) echo "class='rp'"; else  if ($user_rate<0)  echo "class='rm'";?>><?php echo $user_rate;?>
+        </span><?php if ($allow_post) { ?><a href='work/newpost'>Новый пост</a>
+            <?php } if ($allow_blog) {?> / <a href='work/newblog'>блог</a><?php }?>. Ваши <a href='work/myblog'>блоги</a> и
+    
+        <a href='work/pmls' title='<?php echo $not_readed." / ".$mail;?>'>ЛС</a><span class='grey' <?php if ($not_readed>0) { echo "style='color:#E85252;'"; }?> > (<?php echo $not_readed."/".$mail;?>)</span> </div>
+    <div id='inout'><img src='style/img/figure.gif' alt='' />
+        <form id='out' method='post' action='<?php echo $site;?>'><input type='hidden' name='un' value='1' />
+            <input style="cursor: pointer;"  id='outb' type='submit' value='Выйти' /></form></div>
+            <?php } else {?>
+    <a href='register'>Зарегистрироваться</a></div>
+<div id='inout'><a id='lgin' href='login/<?php echo $current_url; ?>'>Войти</a></div>
+<?php }?>
 </div>
 </div>
 </div>
