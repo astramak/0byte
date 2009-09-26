f5127288e36dbf3eff58409c6bf71e48c85e943d
nvbn.rm <nvbn.rm@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-09-26 10:39:45 +0000

    bug fix

--- trunk/style/templates/post_list/user.tpl.php
+++ trunk/style/templates/post_list/user.tpl.php
@@ -2,7 +2,7 @@
     <?php if ($avatar) { ?>
         <img  src='<?php echo $avatar_url; ?>' style='float:left;' alt='' />
     <?php } ?>
-<span class='bnm'><a href='user/<?php echo $name; ?>/'><?php echo $name; ?></a></span><span class='rate'>
+<span class='bnm'><a href='user/<?php echo $name; ?>/'><?php echo $name; ?></a></span><?php if (!$no_user_rate) { ?><span class='rate'>
     <noindex><a rel='nofollow' class='ratep' href='<?php echo $ratep_url; ?>'>+</a></noindex>
     <span id='ru<?php echo $name; ?>'>
         <?php if ($rate>0) { ?>
@@ -11,4 +11,4 @@
         ?><span class='rm'><?php echo $rate; ?></span<?php
         } else echo 0;?>
 </span><noindex><a rel='nofollow' class='ratem' href='<?php echo $ratem_url; ?>'>
-        &ndash;</a></noindex></span></div>
+        &ndash;</a></noindex></span><?php } ?></div>
