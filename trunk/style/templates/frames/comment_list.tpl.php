b7704aaed81e62fa6138f6b57b2946934f92203b
nvbn.rm <nvbn.rm@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-10-31 22:22:53 +0000

    new lenta (work, but not fully complete)

--- trunk/style/templates/frames/comment_list.tpl.php
+++ trunk/style/templates/frames/comment_list.tpl.php
@@ -17,28 +17,9 @@
             <a class='ratem' href='<?php echo $ratem_url; ?>'>&ndash;</a>
         </span>
 </div>
- <?php foreach ($comments as $comment) { ?>
-     <div class='ctop'>
-         <?php if ($avatar) { ?>
-            <img class='cauth' src='<?php echo $avatar_url; ?>' style='float:left;' alt='' />
-         <?php } else { ?>
-            <img class='cnoauth' src='style/img/figure.gif' alt="" />
-         <?php } ?>
-            <span class='date'><?php echo $comment['date']; ?></span>
-      <span class='cauth'>&nbsp;<a href='<?php echo $comment['blog_url']; ?>'><?php echo $comment['blog']; ?></a>  &#8212;  
-              <a href='<?php echo $comment['url']; ?>'><?php echo $comment['post_title']; ?></a></span>
-            <span class='crate rateonly'>
-             <?php if ($comment['rate']>0) { ?>
-                <span class='rp'><?php echo $comment['rate'];?></span>
-            <?php } else if ($comment['rate']<0) {?>
-                <span class='rm'><?php echo $comment['rate'];?></span>
-            <?php } else { ?>
-                <?php echo $comment['rate'];?>
-            <?php } ?>
-           </span>
-     </div>
-<div class='ctext'><?php echo $comment['text']; ?></div>
-<?php } ?>
+ <?php foreach ($comments as $comment) {
+    include('comment2.tpl.php');
+ } ?>
 <?php } else {?>
 <br/>Положите начало беседе :)<br/>
 <?php } ?>
