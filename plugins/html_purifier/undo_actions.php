0d27fa688b160fcdf34fc683a5a71475f842d88e
nvbn.rm <nvbn.rm@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-11-16 12:14:53 +0000

    fix http://code.google.com/p/0byte/issues/detail?id=83

--- plugins/html_purifier/undo_actions.php
+++ plugins/html_purifier/undo_actions.php
@@ -3,6 +3,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+$text = preg_replace ("/\[code\](.*?)\[\/code\]/is", '[code]$1[/code]', $text);
+$text = preg_replace ("/\[code=([^\]]*)](.*?)\[\/code\]/is", '<code lang="$1">$2</code>', $text);
 $text = preg_replace ('/\<div class\="quote"\>(.*?)\<\/div\>/is', '<quote>$1</quote>', $text);
 $text=str_replace('<br />', "\n", $text);
 ?>
