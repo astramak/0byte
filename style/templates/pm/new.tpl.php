b1c506200a9c224729b7e2eaf3e79442b5751153
nvbn.rm <nvbn.rm@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2009-12-18 21:12:30 +0000

    new pm form without reload page from user page

--- style/templates/pm/new.tpl.php
+++ style/templates/pm/new.tpl.php
@@ -5,12 +5,12 @@
 	
 	<input type="text" class="post-pm" name="to" value="<?php echo @$name; ?>" /><span class="grey"> - получатель</span><br>
 
-    <input type="text" class="post-pm" name="title" /><span class="grey"> - заголовок</span>
+    <input type="text" id="pm_title" class="post-pm" name="title" /><span class="grey"> - заголовок</span>
 	
 
 	<div id="rd" class='inpt'></div>
 	<textarea onkeypress="do_key(this.form,'pm',event);"
 			onkeydown="if('\v'=='v') {do_key(this.form,'pm',event);}" name="text"
 			rows="15" cols="70"></textarea>
-
+<br />
 <input type="submit" class="tag_w6" value="Отправить!" /></form>
