6b98666c668b005c3e3de0097acf1b27a7c2fe35
astramak <astramak@73b44744-e4b0-11dd-9d8c-a70aa0a93a20>
2010-02-26 19:02:36 +0000

    fix tabindex on login page

--- style/templates/frames/login.tpl.php
+++ style/templates/frames/login.tpl.php
@@ -16,16 +16,16 @@
     <a href="looz/">восстановление</a>
 	</td></tr></table>
 
-	<br><input type="checkbox" name="zap" id="zap" value="1" /><span class="grey"><label for="zap">запомнить данные</label></span>
+	<br><input type="checkbox" name="zap" id="zap" value="1" tabindex="3"/><span class="grey"><label for="zap">запомнить данные</label></span>
 	
 <br><br>
 	
 		
 
 		
-	<input type="submit" value="Войти" class="tag_w6" tabindex="3" /> <?php
+	<input type="submit" value="Войти" class="tag_w6" tabindex="4" /> <?php
 						if ($js) {
-							?><input type='button' onblur='onBlur(this)' onfocus='onFocus(this)' onclick='unlogin()' value='&larr; Вернуться' class="tag_w4"/>
+							?><input type='button' onblur='onBlur(this)' onfocus='onFocus(this)' onclick='unlogin()' value='&larr; Вернуться' class="tag_w4" tabindex="5"/>
 					<?php }	?>
 
 		</form>
